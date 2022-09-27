63ea488ee62239f487ab0f1897b1bc12f1098cb6
fortuna <avidia38@gmail.com>
2022-09-27 18:39:33 +0900

    feat: company_listing_screen with viewModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:clean_stock_app/data/source/local/company_listing_entity.dart';
+import 'package:clean_stock_app/presentation/company_listings/company_listings_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'util/color_schemes.dart';
@@ -28,55 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: darkColorScheme,
       ),
       themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CompanyListingsScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-      
-        
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-                                    _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-                            return Scaffold(
-      appBar: AppBar(
-                        title: Text(widget.title),
-      ),
-      body: Center(
-                        child: Column(
-                                                                                                                                                      mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),     );
   }
 }
