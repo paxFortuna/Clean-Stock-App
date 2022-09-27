b1ce6e153a1e4810646570e58c10e9b656522c69
fortuna <avidia38@gmail.com>
2022-09-27 12:47:50 +0900

    feat: company_listing_entity & stock_dao with Hive #1

---
+++ lib/data/source/local/stock_dao.dart
@@ -0,0 +1,28 @@
+// 데이터베이스에 접근하는 기능을 모아둠
+import 'package:hive/hive.dart';
+
+import 'company_listing_entity.dart';
+
+class StockDao {
+  //키값은 상수로 정의해서 사용하면 실수를 줄일 수 있다.
+  static const companyListing = 'companyListing';
+  final box = Hive.box('stock.db');
+
+  // 추가
+  Future<void> insertCompanyListings(
+      List<CompanyListingEntity> companyListingEntity) async {
+    await box.put(StockDao.companyListing, companyListingEntity);
+  }
+
+  // 클리어
+  Future clearComapnyListings() async {
+    await box.close();
+  }
+
+  // 검색
+  Future<List<CompanyListingEntity>> searchCompanyListing(String query) async {
+    final List<CompanyListingEntity> companyListing =
+        box.get(StockDao.companyListing, defaultValue: []);
+    return companyListing;
+  }
+}
