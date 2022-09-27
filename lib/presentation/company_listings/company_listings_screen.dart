112742dd07aa82cc23cf168e85237de6b254646f
fortuna <avidia38@gmail.com>
2022-09-27 20:02:08 +0900

    feat: onAction(onChanged & refeshed)

--- lib/presentation/company_listings/company_listings_screen.dart
+++ lib/presentation/company_listings/company_listings_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:clean_stock_app/presentation/company_listings/company_listings_action.dart';
 import 'package:clean_stock_app/presentation/company_listings/company_listings_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,20 +20,23 @@ class CompanyListingsScreen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
+                onChanged: (query) {
+                  viewModel.onAction(CompanyListingsAction.onSearchQueryChange(query));
+                },
                 decoration: InputDecoration(
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
                       color: Theme.of(context).colorScheme.primary,
                       width: 2.0,
                     ),
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(
                       color: Theme.of(context).colorScheme.primary,
                       width: 2.0,
                     ),
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
                   ),
                   labelText: '검색...',
                   labelStyle:
@@ -40,7 +46,9 @@ class CompanyListingsScreen extends StatelessWidget {
             ),
             Expanded(
               child: RefreshIndicator(
-                onRefresh: () async {},
+                onRefresh: () async {
+                  viewModel.onAction(const CompanyListingsAction.refresh());
+                },
                 child: ListView.builder(
                   itemCount: state.companies.length,
                   itemBuilder: (context, index) {
--- lib/presentation/company_listings/company_listings_view_model.dart
+++ lib/presentation/company_listings/company_listings_view_model.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:clean_stock_app/presentation/company_listings/company_listings_action.dart';
 import 'package:clean_stock_app/presentation/company_listings/company_listings_state.dart';
 import 'package:flutter/foundation.dart';
 
@@ -9,8 +12,24 @@ class CompanyListingsViewModel with ChangeNotifier {
   var _state = const CompanyListingsState(); // 상태 변수이기에 final은 안됨
   CompanyListingsState get state => _state;
 
+  // debounce .사용후 지워져야 하기에 널러벌 해야 함
+  //Timer? _debounce;
+
   CompanyListingsViewModel(this._repository) {
     _getCompanyListings();
+  }
+
+  void onAction(CompanyListingsAction action) {
+    action.when(refresh: () => _getCompanyListings(fetchFromRemote: true),
+    onSearchQueryChange: (query){
+      //if(_debounce?.isActive ?? false){
+      //  _debounce?.cancel();
+      //} else {
+      //  _debounce = Timer(const Duration(milliseconds: 500), (){
+          _getCompanyListings(query: query);
+     //   });
+     // }
+    },);
   }
 
   // ui에서 veiwModel이 호출되면 아래 메서드를 호출하여 데이터 읽어옴.
@@ -36,7 +55,6 @@ class CompanyListingsViewModel with ChangeNotifier {
         print('리모트 에러: ' + e.toString());
       },
     );
-    notifyListeners();
 
     _state = state.copyWith(
       isLoading: false,
